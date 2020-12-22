b626971e7daf066a013793b1b8b015a9f7e8d163
davidbm99 <davidbermejomorenoblog@gmail.com>
2020-12-22 19:56:36 +0100

    sql
    eliminar id equipo de la tabla usuarios y crear la tabla arbitros

--- comunicaBD/futbol.sql
+++ comunicaBD/futbol.sql
@@ -23,37 +23,37 @@ CREATE TABLE `Equipo` (
     `diferencia_Goles` int(3)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-INSERT INTO `Equipo` (`id_Equipo`, `nombre_Equipo`, `foto_Equipo`, `puntos`, 
-                    `partidos_Jugados`, `victorias`, `empates`, `derrotas`, `goles_Favor`, `goles_Contra`, `diferencia_Goles`) VALUES
-(0, 'Arbitros', null, 0, 0, 0, 0, 0, 0, 0, 0);
-
-
 CREATE TABLE `Usuario` (
     `id_Usuario` int(3) NOT NULL,
-    `identificador`varchar(50) CHARACTER SET utf8 COLLATE utf8_spanish_ci NOT NULL,
-    `contrasenna`varchar(50) CHARACTER SET utf8 COLLATE utf8_spanish_ci NOT NULL,
-    `nombre_Usuario` varchar(45) CHARACTER SET utf8 COLLATE utf8_spanish_ci NOT NULL,
-    `apellidos_Usuario` varchar(80) CHARACTER SET utf8 COLLATE utf8_spanish_ci NOT NULL,
-    `equipoId` int(3) NOT NULL,
+    `identificador` varchar(50) CHARACTER SET utf8 COLLATE utf8_spanish_ci NOT NULL,
+    `contrasenna` varchar(50) CHARACTER SET utf8 COLLATE utf8_spanish_ci NOT NULL,
+    `nombre` varchar(45) CHARACTER SET utf8 COLLATE utf8_spanish_ci NOT NULL,
+    `apellidos` varchar(80) CHARACTER SET utf8 COLLATE utf8_spanish_ci NOT NULL,
     `estado` varchar(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-
-INSERT INTO `Usuario` (`id_Usuario`,`identificador`,`contrasenna`, `nombre_Usuario`, `apellidos_Usuario`, `equipoId`, `estado`) VALUES
-    (1, 'ggonzalez','abcd3','Mario', 'Gil Gonzalez', 0, 'arbirto'),
-    (2, 'emarquez','abcd4','Eric', 'Marquez Perez', 0, 'arbitro');
-
 
 CREATE TABLE `Partido` (
     `id_Partido` int(3) NOT NULL,
     `id_Equipo_Local` int(3) NOT NULL,
     `id_Equipo_Visitante` int(3) NOT NULL,
-    `fecha` timestamp,
+    `fecha` timestamp NOT NULL,
+    `id_Arbitro` int(2) NOT NULL,
     `gol_Local` int(2),
     `gol_Visitante` int(2),
     `ganador` varchar(45)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+CREATE TABLE `Arbitro` (
+    `id_Arbitro` int(2) NOT NULL,
+    `identificador` varchar(50) CHARACTER SET utf8 COLLATE utf8_spanish_ci NOT NULL,
+    `contrasenna` varchar(50) CHARACTER SET utf8 COLLATE utf8_spanish_ci NOT NULL,
+    `nombre` varchar(45) CHARACTER SET utf8 COLLATE utf8_spanish_ci NOT NULL,
+    `apellidos` varchar(45) CHARACTER SET utf8 COLLATE utf8_spanish_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+INSERT INTO `Arbitro` (`id_arbitro`, `identificador`, `contrasenna`, `nombre`, `apellidos`) VALUES
+    (1, 'mggonzalez', 'abcd', 'Mario', 'Gil Gonzalez'),
+    (1, 'emperez', '1234', 'Eric', 'Marquez Perez');
 
 ALTER TABLE `Equipo`
     ADD PRIMARY KEY (`id_Equipo`);
@@ -61,8 +61,6 @@ ALTER TABLE `Equipo`
 
 ALTER TABLE `Usuario`
     ADD PRIMARY KEY (`id_Usuario`);
-    /*ADD KEY `fk_equipoIdIdx` (`equipoId`);*/
-
 
 ALTER TABLE `Equipo`
     MODIFY `id_equipo` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
@@ -73,11 +71,8 @@ ALTER TABLE `Usuario`
 
 
 ALTER TABLE `Partido`
-    ADD CONSTRAINT `fk_equipo_localId` FOREIGN KEY (`id_Equipo_Local`) REFERENCES `Equipo` (`id_Equipo`),
-    ADD CONSTRAINT `fk_equipoI_visitanted` FOREIGN KEY (`id_Equipo_Visitante`) REFERENCES `Equipo` (`id_Equipo`);
+    ADD CONSTRAINT `fk_equipo_localId` FOREIGN KEY (`id_Equipo_Local`) REFERENCES `Equipo` (`id_Equipo`) ON DELETE CASCADE ON UPDATE CASCADE,
+    ADD CONSTRAINT `fk_equipoI_visitanted` FOREIGN KEY (`id_Equipo_Visitante`) REFERENCES `Equipo` (`id_Equipo`) ON DELETE CASCADE ON UPDATE CASCADE;
 
-
-ALTER TABLE `Usuario`
-    ADD CONSTRAINT `fk_equipoId` FOREIGN KEY (`equipoId`) REFERENCES `Equipo` (`id_Equipo`) ON DELETE CASCADE ON UPDATE CASCADE;
 SET FOREIGN_KEY_CHECKS=1;
 COMMIT;
