4ec61637b5b7eae969415f7ef30b2e1d9c1e4a28
Yaser Haddad <mohamedyaserhaddad@gmail.com>
2020-12-27 18:46:45 +0100

    DAO

---
+++ comunicaBD/DAO.php
@@ -0,0 +1,54 @@
+<?php
+
+require_once "varios.php";
+class DAO
+{
+    private static $pdo = null;
+
+    private static function obtenerPdoConexionBD()
+    {
+        $servidor = "localhost";
+        $identificador = "root";
+        $contrasenna = "";
+        $bd = "futbol"; // Schema
+        $opciones = [
+            PDO::ATTR_EMULATE_PREPARES => false, // Modo emulación desactivado para prepared statements "reales"
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION, // Que los errores salgan como excepciones.
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC, // El modo de fetch que queremos por defecto.
+        ];
+
+        try {
+            $pdo = new PDO("mysql:host=$servidor;dbname=$bd;charset=utf8", $identificador, $contrasenna, $opciones);
+        } catch (Exception $e) {
+            error_log("Error al conectar: " . $e->getMessage());
+            exit("Error al conectar" . $e->getMessage());
+        }
+
+        return $pdo;
+    }
+
+    private static function ejecutarConsulta(string $sql, array $parametros): array
+    {
+        if (!isset(self::$pdo)) self::$pdo = self::obtenerPdoConexionBd();
+
+        $select = self::$pdo->prepare($sql);
+        $select->execute($parametros);
+        $rs = $select->fetchAll();
+
+        return $rs;
+    }
+
+    // Devuelve:
+    //   - null: si ha habido un error
+    //   - 0, 1 u otro número positivo: OK (no errores) y estas son las filas afectadas.
+    private static function ejecutarActualizacion(string $sql, array $parametros): ?int
+    {
+        if (!isset(self::$pdo)) self::$pdo = self::obtenerPdoConexionBd();
+
+        $actualizacion = self::$pdo->prepare($sql);
+        $sqlConExito = $actualizacion->execute($parametros);
+
+        if (!$sqlConExito) return null;
+        else return $actualizacion->rowCount();
+    }
+}
